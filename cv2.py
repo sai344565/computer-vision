fc23d601e685f30c6c28fdfd9e079254963e1be1
sai344565 <113580333+sai344565@users.noreply.github.com>
2023-10-19 13:47:06 +0530

    Add files via upload

---
+++ cv2.py
@@ -0,0 +1,9 @@
+import cv2
+image_path = "C:/Users/Akash Reddy/Downloads/cv2.jpg"
+image = cv2.imread(image_path)
+blurred_image = cv2.GaussianBlur(image, (5, 5), 0)
+cv2.imshow('Original Image', image)
+cv2.imshow('Blurred Image', blurred_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()	
+
